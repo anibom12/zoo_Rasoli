a60e63b4069369d626cde4df392f2bd39f9c3c80
Fatemeh Rasoli <rasolimobina13@gmail.com>
2025-01-25 22:28:00 +0330

    Add files via upload

---
+++ Zoo/IAnimalManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zoo;
+
+public interface IAnimalManager
+{
+    void AddAnimals(Animal animal);
+    void ViewAllAnimals();
+    Animal GetAnimalByID(int id);
+    void RemoveAnimal(int id);
+    void EditAnimal(int id);
+}
---
+++ Zoo/UserInput.cs
@@ -0,0 +1,51 @@
+﻿public static class UserInput
+{
+    public static string GetValidChoice()
+    {
+        string choice;
+        while (true)
+        {
+            Console.WriteLine("Select an option:");
+            Console.WriteLine("1. Add Animal");
+            Console.WriteLine("2. View All Animals");
+            Console.WriteLine("3. View Animal Details");
+            Console.WriteLine("4. Remove Animal");
+            Console.WriteLine("5. Edit Animal");
+            Console.WriteLine("6. Exit");
+
+            choice = Console.ReadLine();
+
+            if (new[] { "1", "2", "3", "4", "5", "6" }.Contains(choice))
+            {
+                break;
+            }
+
+            Console.WriteLine("Invalid choice. Please select one of the options below.");
+        }
+
+        return choice;
+    }
+
+    public static bool GetYesNoInput(string prompt)
+    {
+        string input;
+        while (true)
+        {
+            Console.WriteLine(prompt + " (yes/no):");
+            input = Console.ReadLine().Trim().ToLower();
+
+            if (input == "yes")
+            {
+                return true;
+            }
+            else if (input == "no")
+            {
+                return false;
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter 'yes' or 'no'.");
+            }
+        }
+    }
+}
